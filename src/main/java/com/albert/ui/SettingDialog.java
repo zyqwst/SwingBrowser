b4f9256670974b3c4aad92c681cd82f956ce65c0
albert <790321193@qq.com>
2018-01-13 00:31:08 +0800

    ---

--- src/main/java/com/albert/ui/SettingDialog.java
+++ src/main/java/com/albert/ui/SettingDialog.java
@@ -13,6 +13,14 @@ import javax.swing.JDialog;
 import com.albert.AppContext;
 import com.albert.utils.MessageUtil;
 import com.teamdev.jxbrowser.chromium.Browser;
+import com.teamdev.jxbrowser.chromium.JSValue;
+import com.teamdev.jxbrowser.chromium.WebStorage;
+import com.teamdev.jxbrowser.chromium.events.ConsoleEvent;
+import com.teamdev.jxbrowser.chromium.events.ConsoleListener;
+import com.teamdev.jxbrowser.chromium.events.LoadAdapter;
+import com.teamdev.jxbrowser.chromium.events.LoadEvent;
+import com.teamdev.jxbrowser.chromium.events.ScriptContextAdapter;
+import com.teamdev.jxbrowser.chromium.events.ScriptContextEvent;
 import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 
 /** 
@@ -22,14 +30,13 @@ import com.teamdev.jxbrowser.chromium.swing.BrowserView;
 * @date 2018年1月11日 上午9:23:49 
 *  
 */
-public class SettingDialog extends BaseDialog {
+public class SettingDialog extends BaseDialog implements ConsoleListener{
 	public SettingDialog(Window window) {
 		super(window);
 		Browser browser = new Browser();
-	     BrowserView view = new BrowserView(browser);
-	     String url = getClass().getResource("/pages/print.html").toString();
-	     System.out.println(url);
-	     browser.loadURL(url);
+	    BrowserView view = new BrowserView(browser);
+	    String url = getClass().getResource("/pages/print.html").toString();
+	    browser.loadURL(url);
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -40,21 +47,31 @@ public class SettingDialog extends BaseDialog {
 				.addComponent(view, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 		);
 		getContentPane().setLayout(groupLayout);
-		 
+		browser.addConsoleListener(this);
+		browser.addScriptContextListener(new ScriptContextAdapter() {
+	           @Override
+	           public void onScriptContextCreated(ScriptContextEvent event) {
+	               Browser browser = event.getBrowser();
+	               JSValue window = browser.executeJavaScriptAndReturnValue("window");
+	               window.asObject().setProperty("settingContext", new SettingBridge());
+	           }
+	     });
+		browser.addLoadListener(new LoadAdapter() {
+            @Override
+            public void onDocumentLoadedInMainFrame(LoadEvent event) {
+                Browser browser = event.getBrowser();
+                WebStorage webStorage = browser.getSessionWebStorage();
+                webStorage.setItem("myKey", "马勒戈壁");
+            }
+        });
 	}
 	@Override
 	public void customizedInit() {
 		super.customizedInit();
 		setTitle(AppContext.getInstance().getI18nMap().get(MessageUtil.settingMenu));
 	}
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = -1597310160254338256L;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -67,5 +84,14 @@ public class SettingDialog extends BaseDialog {
 				}
 			}
 		});
+	}
+	public class SettingBridge{
+		public void selectRow(String json){
+			System.out.println(json);
+		}
+	}
+	@Override
+	public void onMessage(ConsoleEvent event) {
+		System.out.println(event.getMessage());
 	}
 }
